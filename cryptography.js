f4d5bd6684e230f87298f3808edbc571d8046402
cblgh <cblgh@cblgh.org>
2023-09-20 09:54:32 +0200

    create util.js to hold {de,}serializeKeypair functions

--- cryptography.js
+++ cryptography.js
@@ -46,27 +46,6 @@ function randomBytes (len) {
   return buf
 }
 
-// takes the json structure produced by generateKeypair and returns a serialized string representation 
-// where the buffers have been correctly serialized as hex strings
-function serializeKeypair(kp) {
-  const json = { 
-    publicKey: b4a.toString(kp.publicKey, "hex"), 
-    secretKey: b4a.toString(kp.secretKey, "hex"), 
-  }
-  return JSON.stringify(json)
-}
-
-// takes the json string representation returned by serializeKeypair and converts 
-// into the structure produced by generateKeypair()
-function deserializeKeypair(input) {
-  const json = JSON.parse(input)
-  const kp = {
-    publicKey: b4a.from(json.publicKey, "hex"), 
-    secretKey: b4a.from(json.secretKey, "hex"), 
-  }
-  return kp
-}
-
 // buf is a buffer that is exactly as large as the message (i.e. no overshooting placeholder bytes after message
 // payload)
 function sign (buf, secretKey) {
@@ -104,8 +83,6 @@ function hash(buf) {
 module.exports = {
   generateReqID,
   generateKeypair,
-  serializeKeypair,
-  deserializeKeypair,
   randomBytes,
   hash,
   sign,
---
+++ util.js
@@ -0,0 +1,26 @@
+const b4a = require("b4a")
+// takes the json structure produced by generateKeypair and returns a serialized string representation 
+// where the buffers have been correctly serialized as hex strings
+function serializeKeypair(kp) {
+  const json = { 
+    publicKey: b4a.toString(kp.publicKey, "hex"), 
+    secretKey: b4a.toString(kp.secretKey, "hex"), 
+  }
+  return JSON.stringify(json)
+}
+
+// takes the json string representation returned by serializeKeypair and converts 
+// into the structure produced by generateKeypair()
+function deserializeKeypair(input) {
+  const json = JSON.parse(input)
+  const kp = {
+    publicKey: b4a.from(json.publicKey, "hex"), 
+    secretKey: b4a.from(json.secretKey, "hex"), 
+  }
+  return kp
+}
+
+module.exports = {
+  serializeKeypair,
+  deserializeKeypair
+}
