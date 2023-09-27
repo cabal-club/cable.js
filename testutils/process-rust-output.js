2be154897bc0531c6bef60d2535152851d2f2df5
cblgh <cblgh@cblgh.org>
2023-09-27 11:16:58 +0200

    reuse compliance

--- testutils/process-rust-output.js
+++ testutils/process-rust-output.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 the cabal-club authors
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 const fs = require("fs")
 
 fs.readFile("./rust-output.txt", (err, data) => {
